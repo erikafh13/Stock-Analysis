ada9ff5fdf2252dc9e4630cad4e93988338d11aa
erikafh13 <127170590+erikafh13@users.noreply.github.com>
2026-04-22 16:51:09 +0700

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -31,6 +31,7 @@ from .analysis import (
     calculate_add_stock_v2,
     calculate_suggested_po_v2,
     calculate_all_summary_v2,
+    calculate_persentase_stock,
     # Donor
     calculate_donor_distribution,
     get_status_stock,
